e50bf6991e87b91b474e8fea92ff2f6824a2f855
sarthaksood9 <sarthaksood9@gmail.com>
2023-10-08 14:38:38 +0530

    new changes

--- script.js
+++ script.js
@@ -32,24 +32,25 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < movies.length; i += 4) {
       const movieGroup = document.createElement("div");
       movieGroup.classList.add("movie-group");
+    
 
       for (let j = i; j < i + 4 && j < movies.length; j++) {
         const movie = movies[j];
         const movieDiv = document.createElement("div");
         movieDiv.classList.add("movie");
-        movieDiv.style.border = "1px solid #007bff";
-        movieDiv.style.padding = "10px";
-        movieDiv.style.height = "100%";
-        movieDiv.style.marginBottom = "30px";
-        movieDiv.style.backgroundColor = "#f7d916c6";
-        movieDiv.style.textAlign = "center";
-        movieDiv.style.width = "100%";
-        movieDiv.style.boxSizing = "border-box";
-        movieDiv.style.display = "flex";
-        movieDiv.style.flexDirection = "column";
-        movieDiv.style.alignItems = "center";
-        movieDiv.style.transform = "scale(1.1)";
-        movieDiv.style.height = "400px";
+        // movieDiv.style.border = "1px solid #007bff";
+        // movieDiv.style.padding = "10px";
+        // movieDiv.style.height = "100%";
+        // movieDiv.style.marginBottom = "30px";
+        // movieDiv.style.backgroundColor = "#f7d916c6";
+        // movieDiv.style.textAlign = "center";
+        // movieDiv.style.width = "100%";
+        // movieDiv.style.boxSizing = "border-box";
+        // movieDiv.style.display = "flex";
+        // movieDiv.style.flexDirection = "column";
+        // movieDiv.style.alignItems = "center";
+        // movieDiv.style.transform = "scale(1.1)";
+        // movieDiv.style.height = "400px";
 
         movieDiv.innerHTML = `
   <div class="movie-inner">
